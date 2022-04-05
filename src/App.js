93a0237243318e7651d35612be4bbd39e8cdbd65
mad-max-fury <chrisnonso8@gmail.com>
2022-04-05 18:30:22 +0100

    dev: added button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./component/button";
+import { colors } from "./colors";
 
 function App() {
   return (
@@ -9,14 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* <Button
+          bgColor={colors.primary_cadium_violet}
+          dir={"right"}
+          src={"images/google.svg"}
+          filled={false}
+        /> */}
       </header>
     </div>
   );
---
+++ src/component/button/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { colors } from "../../colors";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+const Button = ({ src, dir, filled, bgColor }) => {
+  return (
+    <>
+      (
+      <ButtonWrap dir={dir} filled={filled} bgColor={bgColor}>
+        {src && <Icon src={src} alt={"icon"} />}
+        <p>Button</p>
+        <IconArrow>
+          {dir === "left" ? <AiOutlineArrowLeft /> : <AiOutlineArrowRight />}
+        </IconArrow>
+      </ButtonWrap>
+      )
+    </>
+  );
+};
+
+export default Button;
+
+const ButtonWrap = styled.div`
+  border: 1px solid
+    ${({ filled, bgColor }) => (filled === true ? bgColor : "none")};
+  background-color: ${({ filled, bgColor }) =>
+    filled === true ? "none" : bgColor};
+  width: fit-content;
+  display: flex;
+  flex-direction: ${({ dir }) => (dir === "left" ? "row-reverse" : "row")};
+  gap: 1rem;
+  align-items: center;
+  padding: 0.5rem 24px;
+  border-radius: 24px;
+  font-weight: 700;
+  font-size: 14px;
+  color: ${({ filled, bgColor }) => (filled === true ? bgColor : colors.white)};
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    background-color: ${({ filled, bgColor }) =>
+      filled === true ? bgColor : "unset"};
+    color: ${({ filled, bgColor }) =>
+      filled === true ? colors.white : bgColor};
+    border: 1px solid
+      ${({ filled, bgColor }) => (filled === true ? "unset" : bgColor)};
+    padding: 0.5rem 28px;
+  }
+`;
+
+const Icon = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+const IconArrow = styled.div`
+  display: flex;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+`;
