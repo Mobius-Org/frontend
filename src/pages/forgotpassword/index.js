6be54c400dea72126f6b11308b33f6b27731721c
mad-max-fury <chrisnonso8@gmail.com>
2022-04-19 08:52:50 +0100

    updated style.js import path

--- src/pages/forgotpassword/index.js
+++ src/pages/forgotpassword/index.js
@@ -3,14 +3,7 @@ import { colors } from "../../colors";
 import Button from "../../component/button";
 import InputField from "../../component/inputfield";
 import { ImGoogle } from "react-icons/im";
-import {
-  CtaWrap,
-  HeadingText,
-  InputWrap,
-  Wrap,
-  Wrap2,
-  Wrapper,
-} from "./style.js";
+import { CtaWrap, HeadingText, InputWrap, Wrap, Wrap2, Wrapper } from "./style";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ErrorMessage, InputWrapDiv } from "../signin/style";
 const ForgotPassword = () => {
