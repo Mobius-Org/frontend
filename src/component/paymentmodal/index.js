88d5f8352337b0e20a784776d9deb246bbccb76a
mad-max-fury <chrisnonso8@gmail.com>
2022-04-30 15:59:42 +0100

    completed enrollment integration

--- src/component/paymentmodal/index.js
+++ src/component/paymentmodal/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { colors } from "../../colors";
